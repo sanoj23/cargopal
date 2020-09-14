06b99b708ff3ed4e598f86842a2ebd3b70178901
Umair Feroze <umayir10@gmail.com>
2020-09-14 14:12:47 +0530

    Reset Project
    Project Repo Reset

--- AdminScreens/AdminHomePage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const AdminHome = (props) => ({});
-
-export default AdminHome;
--- ShipOwnerScreens/AgentHomePage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const AgentHome = ({ navigation }) => ({});
-
-export default AgentHome;
