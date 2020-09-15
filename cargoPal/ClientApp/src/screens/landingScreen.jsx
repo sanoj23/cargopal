0b881ae7bd18d2a29831aa0e234b441377f64aaa
ShenukiPerera <35036780+ShenukiPerera@users.noreply.github.com>
2020-09-15 13:25:10 +0530

    Update landingScreen.jsx

--- cargoPal/ClientApp/src/screens/landingScreen.jsx
+++ cargoPal/ClientApp/src/screens/landingScreen.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
-import { Image, Button } from 'react-bootstrap';
+import React from "react";
+import { Image, Button } from "react-bootstrap";
 
-import { useHistory } from 'react-router-dom';
-import Screen from '../components/screen';
+import { useHistory } from "react-router-dom";
+import Screen from "../components/screen";
+
+import LandingImage from "../assets/commons/LandingImage.jpg";
 
 export default function LandingScreen(props) {
   let history = useHistory();
   const windowHeight = window.innerHeight;
 
   return (
-    <div style={{ position: 'relative' }}>
-      <h1>This is the page u need to do</h1>
+    <div
+      style={{
+        backgroundImage: `url(${LandingImage})`,
+        height: windowHeight,
+        overflow: "hidden",
+        backgroundSize: "cover",
+        textAlign: "center",
+      }}
+    >
+      <div
+        style={{
+          borderStyle: "solid",
+          display: "inline-block",
+        }}
+      >
+        <Button
+          style={{ margin: 30 }}
+          varient="Customer"
+          size="lg"
+          onClick={() => history.push("/login")}
+        >
+          Customer
+        </Button>
+        <Button
+          style={{ margin: 30 }}
+          varient="Agent"
+          size="lg"
+          onClick={() => history.push("/login")}
+        >
+          Agent
+        </Button>
+      </div>
     </div>
   );
 }
