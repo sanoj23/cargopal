5ce9d4c28d580d392bb928a3b33c6969ff271dff
ShenukiPerera <35036780+ShenukiPerera@users.noreply.github.com>
2020-09-21 12:59:48 +0530

    fake data

---
+++ cargoPal/ClientApp/src/fakeData.jsx
@@ -0,0 +1,44 @@
+let Shipments = [
+  {
+    shipmentId: 21,
+    userId: 11,
+    origin: "colombo",
+    destination: "chennai",
+    startdate: "01/10/2020",
+    enddate: "03/10/2020",
+    capacity: 100,
+    status: "Approved",
+  },
+
+  {
+    shipmentId: 22,
+    userId: 12,
+    origin: "colombo",
+    destination: "singapore",
+    startdate: "05/10/2020",
+    enddate: "08/10/2020",
+    capacity: 50,
+    status: "Approved",
+  },
+
+  {
+    shipmentId: 23,
+    userId: 13,
+    origin: "colombo",
+    destination: "malaysia",
+    startdate: "01/09/2020",
+    enddate: "02/09/2020",
+    capacity: 30,
+    status: "Approved",
+  },
+  {
+    shipmentId: 24,
+    userId: 14,
+    origin: "colombo",
+    destination: "maldives",
+    startdate: "05/11/2020",
+    enddate: "08/11/2020",
+    capacity: 300,
+    status: "Approved",
+  },
+];
