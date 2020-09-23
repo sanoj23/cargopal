95efa8d4f16da30d2c2b7d209954c88c303b3a1a
Umair Feroze <umayir10@gmail.com>
2020-09-23 11:11:59 +0530

    Orders Controller completed

---
+++ cargoPal/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using CargoPal.Data;
+
+namespace CargoPal.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _service;
+
+        public OrdersController(IOrderService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetOrders()
+        {
+            try
+            {
+                var allOrders = _service.GetOrders();
+                return Ok(allOrders);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+    }
+}
