7a8a2b21a1fcc0c4a01d00efe2eabda047755cb6
Umair Feroze <umayir10@gmail.com>
2020-09-25 06:50:44 +0530

    Shipment redux completed

--- cargoPal/ClientApp/src/actions/shipmentAction.js
+++ cargoPal/ClientApp/src/actions/shipmentAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { shipmentsConstants } from '../constants/shipmentConstants';
 
-// get all shipments
+// Get all shipments
 const GetShipmentSuccess = (payload) => ({
   type: shipmentsConstants.GET_SHIPMENTS_SUCCESS,
   payload,
@@ -22,7 +22,109 @@ export const GetShipment = () => (dispatch) => {
     });
 };
 
-// get shipment by Id
-// get shipment by user Id
+// Get shipment by Id
+const GetShipmentByIdSuccess = (payload) => ({
+  type: shipmentsConstants.SHIPMENTS_BY_ID_SUCCESS,
+  payload,
+});
+const GetShipmentByIdFailure = (payload) => ({
+  type: shipmentsConstants.SHIPMENTS_BY_ID_FAILURE,
+  payload,
+});
+export const GetShipmentById = (shipmentId) => (dispatch) => {
+  dispatch({ type: shipmentsConstants.SHIPMENTS_BY_ID_REQUEST });
+  return axios
+    .get(`api/shipments/${shipmentId}`)
+    .then((res) => {
+      dispatch(GetShipmentByIdSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(GetShipmentByIdFailure(err));
+    });
+};
+
+// Get shipment by user Id
+const GetShipmentByUserSuccess = (payload) => ({
+  type: shipmentsConstants.SHIPMENTS_BY_USER_SUCCESS,
+  payload,
+});
+const GetShipmentByUserFailure = (payload) => ({
+  type: shipmentsConstants.SHIPMENTS_BY_USER_FAILURE,
+  payload,
+});
+export const GetShipmentByUser = (userId) => (dispatch) => {
+  dispatch({ type: shipmentsConstants.SHIPMENTS_BY_USER_REQUEST });
+  return axios
+    .get(`api/shipments/user/${userId}`)
+    .then((res) => {
+      dispatch(GetShipmentByUserSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(GetShipmentByUserFailure(err));
+    });
+};
+
+// Add new shipment
+const AddShipmentSuccess = (payload) => ({
+  type: shipmentsConstants.ADD_SHIPMENTS_SUCCESS,
+  payload,
+});
+const AddShipmentFailure = (payload) => ({
+  type: shipmentsConstants.ADD_SHIPMENTS_FAILURE,
+  payload,
+});
+export const AddShipment = (shipment) => (dispatch) => {
+  dispatch({ type: shipmentsConstants.ADD_SHIPMENTS_REQUEST });
+  return axios
+    .get(`api/shipments/`, shipment)
+    .then((res) => {
+      dispatch(AddShipmentSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(AddShipmentFailure(err));
+    });
+};
+
 // Update shipment
-// delete shipment
+const UpdateShipmentSuccess = (payload) => ({
+  type: shipmentsConstants.UPDATE_SHIPMENTS_SUCCESS,
+  payload,
+});
+const UpdateShipmentFailure = (payload) => ({
+  type: shipmentsConstants.UPDATE_SHIPMENTS_FAILURE,
+  payload,
+});
+export const UpdateShipment = (shipment) => (dispatch) => {
+  dispatch({ type: shipmentsConstants.UPDATE_SHIPMENTS_REQUEST });
+  let { shipmentId } = shipment;
+  return axios
+    .get(`api/shipments/${shipmentId}`, shipment)
+    .then((res) => {
+      dispatch(UpdateShipmentSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(UpdateShipmentFailure(err));
+    });
+};
+
+// Delete shipment
+const DeleteShipmentSuccess = (payload) => ({
+  type: shipmentsConstants.DELETE_SHIPMENTS_SUCCESS,
+  payload,
+});
+const DeleteShipmentFailure = (payload) => ({
+  type: shipmentsConstants.DELETE_SHIPMENTS_FAILURE,
+  payload,
+});
+export const DeleteShipment = (shipment) => (dispatch) => {
+  dispatch({ type: shipmentsConstants.DELETE_SHIPMENTS_REQUEST });
+  let { shipmentId } = shipment;
+  return axios
+    .get(`api/shipments/${shipmentId}`)
+    .then((res) => {
+      dispatch(DeleteShipmentSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(DeleteShipmentFailure(err));
+    });
+};
--- cargoPal/ClientApp/src/reducers/shipmentReducer.js
+++ cargoPal/ClientApp/src/reducers/shipmentReducer.js
@@ -15,6 +15,41 @@ const shipmentReducer = (state = INITIAL_STATE, action) => {
     case shipmentsConstants.GET_SHIPMENTS_FAILURE:
       return { ...state, loading: false, error: action.payload };
 
+    case shipmentsConstants.SHIPMENTS_BY_ID_REQUEST:
+      return { ...state, loading: true };
+    case shipmentsConstants.SHIPMENTS_BY_ID_SUCCESS:
+      return { ...state, loading: false, data: action.payload };
+    case shipmentsConstants.SHIPMENTS_BY_ID_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+
+    case shipmentsConstants.SHIPMENTS_BY_USER_REQUEST:
+      return { ...state, loading: true };
+    case shipmentsConstants.SHIPMENTS_BY_USER_SUCCESS:
+      return { ...state, loading: false, data: action.payload };
+    case shipmentsConstants.SHIPMENTS_BY_USER_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+
+    case shipmentsConstants.ADD_SHIPMENTS_REQUEST:
+      return { ...state, loading: true };
+    case shipmentsConstants.ADD_SHIPMENTS_SUCCESS:
+      return { ...state, loading: false, data: action.payload };
+    case shipmentsConstants.ADD_SHIPMENTS_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+
+    case shipmentsConstants.UPDATE_SHIPMENTS_REQUEST:
+      return { ...state, loading: true };
+    case shipmentsConstants.UPDATE_SHIPMENTS_SUCCESS:
+      return { ...state, loading: false, data: action.payload };
+    case shipmentsConstants.UPDATE_SHIPMENTS_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+
+    case shipmentsConstants.DELETE_SHIPMENTS_REQUEST:
+      return { ...state, loading: true };
+    case shipmentsConstants.DELETE_SHIPMENTS_SUCCESS:
+      return { ...state, loading: false, data: action.payload };
+    case shipmentsConstants.DELETE_SHIPMENTS_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+
     default:
       return state;
   }
