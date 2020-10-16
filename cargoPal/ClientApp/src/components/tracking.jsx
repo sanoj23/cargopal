10289d0bb079aa443f86ec083d599cf5200c85c6
Umair Feroze <umayir10@gmail.com>
2020-10-16 16:39:01 +0530

    Tracking Screen Completed

--- cargoPal/ClientApp/src/components/tracking.jsx
+++ cargoPal/ClientApp/src/components/tracking.jsx
@@ -1,41 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Button, Form } from 'react-bootstrap';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+
+import { GetBookingById } from '../actions/bookingAction';
+
 import FormInput from './form/formInput';
 
-const validationSchema = Yup.object().shape({
-  trackingNumber: Yup.string().required().min(1).max(7).label('Email'),
-});
+class Tracking extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      booking: {},
+    };
+  }
 
-export default function Tracking({ track }) {
-  return (
-    <>
-      <Formik
-        initialValues={{ trackingNumber: '' }}
-        onSubmit={(values) => track(values)}
-        validationSchema={validationSchema}
-      >
-        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
-          <Form>
-            <FormInput
-              id="trackingNumber"
-              name="trackingNumber"
-              type="trackingNumber"
-              label="Tracking Number:"
-              placeholder="Enter Tracking Number"
-              onBlur={() => setFieldTouched('trackingNumber')}
-              onChange={handleChange('trackingNumber')}
-              errors={errors.trackingNumber}
-              touched={touched.trackingNumber}
-            />
+  componentDidUpdate(prevProps) {
+    if (prevProps.bookings.data !== this.props.bookings.data) {
+      this.setState({ booking: this.props.bookings.data });
+      if (!this.props.bookings.error && this.state.booking) {
+        this.props.func(this.props.bookings.data);
+      }
+    }
+  }
 
-            <Button variant="primary" onClick={handleSubmit}>
-              Track
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </>
-  );
+  componentWillUnmount() {
+    this.setState({ booking: null });
+  }
+
+  track = ({ trackingNumber }) => {
+    this.props.GetBookingById(trackingNumber);
+  };
+
+  returnValues = () => {};
+
+  render() {
+    const validationSchema = Yup.object().shape({
+      trackingNumber: Yup.string()
+        .required()
+        .min(1)
+        .max(7)
+        .label('Tracking Number'),
+    });
+    return (
+      <>
+        <Formik
+          initialValues={{ trackingNumber: '' }}
+          onSubmit={(values) => this.track(values)}
+          validationSchema={validationSchema}
+        >
+          {({
+            handleChange,
+            handleSubmit,
+            errors,
+            setFieldTouched,
+            touched,
+          }) => (
+            <Form>
+              <FormInput
+                id="trackingNumber"
+                name="trackingNumber"
+                type="number"
+                label="Booking Number:"
+                placeholder="Enter Booking Number"
+                onBlur={() => setFieldTouched('trackingNumber')}
+                onChange={handleChange('trackingNumber')}
+                errors={errors.trackingNumber}
+                touched={touched.trackingNumber}
+              />
+
+              <Button variant="secondary" onClick={handleSubmit}>
+                Track Booking
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </>
+    );
+  }
 }
+
+const mapStateToProps = ({ bookings }) => ({ bookings });
+export default connect(mapStateToProps, { GetBookingById })(Tracking);
--- cargoPal/ClientApp/src/screens/customer/trackingScreen.jsx
+++ cargoPal/ClientApp/src/screens/customer/trackingScreen.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+import { GetShipmentById } from '../../actions/shipmentAction';
+import { getUserByUserId } from '../../actions/userAction';
 
 import Screen from '../../components/screen';
 import Tracking from '../../components/tracking';
@@ -7,46 +12,126 @@ class TrackingScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userAuth: {},
+      booking: null,
+      shipment: {},
+      user: {},
     };
   }
 
-  componentDidMount() {
-    // reset forms
-    // if (!this.props.location.state.user) {
-    //   const user = this.props.location.state.user;
-    //   if (user && user === 'visitor') {
-    //     this.setState({ userAuth: user });
-    //   }
-    // }
+  componentDidUpdate(prevProps) {
+    if (prevProps.shipments.data !== this.props.shipments.data) {
+      this.setState({ shipments: this.props.shipments.data });
+      if (!this.props.shipments.error) {
+        this.props.getUserByUserId(this.props.shipments.data.userId);
+      }
+    }
+    if (prevProps.users.data !== this.props.users.data) {
+      this.setState({ user: this.props.users.data });
+    }
   }
 
-  componentDidUpdate() {
-    // reenter state of the shipment
-    console.log(this.state.userAuth);
+  componentWillUnmount() {
+    this.setState({ booking: null, shipment: null, user: null });
   }
 
-  requestTracking = (values) => {
-    const track = this.state.items.find(
-      (i) => i.id === parseInt(values.trackingNumber)
+  getInfo = (booking) => {
+    this.setState({ booking: booking });
+    this.props.GetShipmentById(booking.shipmentId);
+  };
+
+  DisplayBooking = () => {
+    const {
+      bookingId,
+      item,
+      instructions,
+      packaging,
+      status,
+      receiverName,
+      receiverPhone,
+    } = this.state.booking;
+
+    return (
+      <>
+        {this.state.booking ? (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#Id</th>
+                <th>Receiver</th>
+                <th>Phone</th>
+                <th>Item</th>
+                <th>Instructions</th>
+                <th>Packaging</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{bookingId}</td>
+                <td>{receiverName}</td>
+                <td>{receiverPhone}</td>
+                <td>{item}</td>
+                <td>{instructions}</td>
+                <td>{packaging}</td>
+                <td>{status}</td>
+              </tr>
+            </tbody>
+          </Table>
+        ) : null}
+      </>
     );
-    console.log(track);
+  };
+
+  DisplayShipment = () => {
+    console.log('check');
+
+    const { companyName, email, phone, address } = this.state.user;
+
+    return (
+      <>
+        {this.state.user && this.state.shipment ? (
+          <>
+            <hr />
+            <h4>{companyName}</h4>
+            <h6>{address}</h6>
+            <h6>{email}</h6>
+            <h6>{phone}</h6>
+
+            <hr />
+            <h4>Shipment Status: {this.props.shipments.data.status}</h4>
+            <hr />
+
+            {this.state.booking ? this.DisplayBooking() : null}
+          </>
+        ) : null}
+      </>
+    );
   };
 
   render() {
     return (
-      <Screen title="Tracking" navbar={`${this.state.userAuth}`}>
-        <div
-          style={{ borderStyle: 'solid', padding: 20, width: 'auto', left: 10 }}
-        >
-          <Tracking track={this.requestTracking.bind(this)} />
-        </div>
+      <Screen
+        title="Tracking"
+        subtitle="Enter the booking number to start tracking"
+        navbar={`${this.state.userAuth}`}
+      >
         <div>
-          <h1>Status: </h1>
+          <Tracking func={this.getInfo} />
+        </div>
+
+        <hr />
+
+        <div>
+          {this.state.booking && this.state.shipment && this.state.user
+            ? this.DisplayShipment()
+            : null}
         </div>
       </Screen>
     );
   }
 }
 
-export default TrackingScreen;
+const mapStateToProps = ({ shipments, users }) => ({ shipments, users });
+export default connect(mapStateToProps, { GetShipmentById, getUserByUserId })(
+  TrackingScreen
+);
